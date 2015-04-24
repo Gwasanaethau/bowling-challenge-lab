a9b8dae4c36600b6fe70a081e6a6316e459f7e63
icicleta <icicleta@gmail.com>
2015-04-24 16:42:28 +0100

    we have two balls, scoreFame and gameFrame

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -5,16 +5,51 @@ describe('Bowling', function() {
     bowling = new Bowling();
   });
 
-  describe('game frame', function() {
+  describe('number of frame', function() {
     it('has 1 frame', function() {
-      expect(bowling.gameFrame()).toBe(1);
+      expect(bowling.frameNumber).toBe(1);
     });
   });
 
-  describe('number of balls', function() {
-    describe('ball 1', function() {
-      it('number of pins knocked down is less than 11', function() {
-        expect(bowling.pinsKnocked()).toBeLessThan(11);
+  describe('number of ball', function() {
+    describe('is One', function() {
+      it('then number of pins knocked down has to be less than 11', function() {
+        expect(bowling.pinsKnocked(10)).toBeLessThan(11);
+      });
+
+      describe('and number of pins down is 10', function(){
+
+        it('then pins left are 0', function(){
+          spyOn(bowling, 'pinsKnocked').and.returnValue(10);
+          expect(bowling.ballOne()).toBe(0);
+        });
+
+        it('roll is Strike and frame is over', function(){
+          spyOn(bowling, 'ballOne').and.returnValue(0);
+          expect(bowling.gameFrame()).toEqual("Strike, frame is over");
+          expect(bowling.frameScore()).toEqual(10);
+        });
+      });
+
+      describe('and number of pins down is less than 10', function(){
+
+        it('the frames continues', function(){
+          spyOn(bowling, 'pinsKnocked').and.returnValue(6);
+          expect(bowling.gameFrame()).toEqual("Frame continues");
+        });
+
+        it('then pins left are 10 minus pins knocked down', function(){
+          spyOn(bowling, 'pinsKnocked').and.returnValue(6);
+          expect(bowling.ballOne()).toBe(4);
+        });
+      });
+    });
+
+    describe('is 2', function() {
+      it('then number of pins knocked down has to be less than pins left in ball 1', function() {
+        spyOn(bowling, 'ballOne').and.returnValue(6);
+        pinsLeft = 10 - bowling.ballOne();
+        expect(bowling.pinsKnocked(pinsLeft)).toBeLessThan(5);
       });
     });
   });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,11 +1,42 @@
 function Bowling() {
+  this.frameNumber = 1;
+  this.startPins = 10;
 }
 
 Bowling.prototype.gameFrame = function() {
-  this.frameNumber = 1;
-  return this.frameNumber;
+  if (this.ballOne() === 0){
+    this.message = "Strike, frame is over";
+  } else {
+    this.message ="Frame continues";
+  }
+  return this.message;
 };
 
-Bowling.prototype.pinsKnocked = function() {
-  return Math.floor(Math.random() * 11);
+Bowling.prototype.ballOne = function() {
+  var pinsDown = this.pinsKnocked(this.startPins);
+  if (pinsDown === 10){
+  var pinsLeft = 0;
+  } else{
+    pinsLeft = this.startPins - pinsDown;
+  };
+  return pinsLeft;
+};
+
+Bowling.prototype.ballTwo = function() {
+  // var pinsDown = this.pinsKnocked(this.startPins);
+  // if (pinsDown === 10){
+  //   return "Strike";
+  // };
+  // this.ballTwo();
+};
+
+Bowling.prototype.frameScore = function() {
+  if (this.message === "Strike, frame is over") {
+    return 10;
+  };
+};
+
+Bowling.prototype.pinsKnocked = function(pins) {
+  range = pins + 1;
+  return Math.floor(Math.random() * range);
 };
